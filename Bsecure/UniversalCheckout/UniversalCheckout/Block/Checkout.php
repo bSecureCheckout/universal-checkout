3f4b7d6bf7bb323af814a55ee3cb0126b0307a91
Khalique <khalique@nextgeni.net>
2020-11-02 13:43:22 +0500

    files structure updated

--- Bsecure/UniversalCheckout/UniversalCheckout/Block/Checkout.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Bsecure\UniversalCheckout\Block;
-
-class Checkout extends \Magento\Checkout\Block\Onepage\Link {
-    // This function is created for testing purpose, you can add more function as per your requirement
-    public function getTest(){
-        return 'Test Button';
-    }
-}
--- Bsecure/UniversalCheckout/UniversalCheckout/view/frontend/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-/*
-* bSecure
-*/
-
-var config = {
-    paths: {
-        "bsecureFront": "Bsecure_UniversalCheckout/js/bsecure-front"
-    }
-}
