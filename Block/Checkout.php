02d91d74220512a67299093483cb3ca85caa40d3
Khalique <khalique@nextgeni.net>
2020-11-20 19:42:54 +0500

    CodeSniffer Changes 8

--- Block/Checkout.php
+++ Block/Checkout.php
@@ -8,12 +8,14 @@ class Checkout extends \Magento\Framework\View\Element\Template
     protected $bsecureHelper;
 
     public function __construct(
-    	\Magento\Framework\View\Element\Template\Context  $context,
+        \Magento\Framework\View\Element\Template\Context  $context,
         \Bsecure\UniversalCheckout\Helper\Data $bsecureHelper,
+        \Magento\Checkout\Block\Onepage\Link $onePageLink,
         array $data = []
     ) {
         
         $this->bsecureHelper = $bsecureHelper;
+        $this->onePageLink = $onePageLink;
         parent::__construct($context, $data);
     }
 
@@ -25,5 +27,15 @@ class Checkout extends \Magento\Framework\View\Element\Template
     public function getBsecureSettings($key)
     {
         return $this->bsecureHelper->getConfig('universalcheckout/general/'.$key);
+    }
+
+    public function getOnePageLink()
+    {
+        return $this->onePageLink;
+    }
+
+    public function getOnePageLinkPath()
+    {
+        return '\Magento\Checkout\Block\Onepage\Link';
     }
 }
