97d00a650bbba330eece6d55f1cde260313f93e7
khalique44 <khalique.ahmed3@gmail.com>
2022-08-29 13:30:00 +0500

    General Updates & Payment Gateway observer updated

--- Observer/OrderComplete.php
+++ Observer/OrderComplete.php
@@ -39,6 +39,7 @@ class OrderComplete implements \Magento\Framework\Event\ObserverInterface
     {
 
         $orderId = $this->checkoutSession->getLastOrderId();
+        //var_dump('orderId',$orderId); die;
         $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
         $order = $this->orderModel->load($orderId);
 
@@ -48,6 +49,7 @@ class OrderComplete implements \Magento\Framework\Event\ObserverInterface
 
         $method = $payment->getMethodInstance();
         $methodTitle = $method->getTitle();
+        $methodCode = $method->getCode();
        
         $status = $this->checkoutSession->getLastOrderStatus();
         $additionalData = $payment->getAdditionalInformation();
@@ -57,12 +59,11 @@ class OrderComplete implements \Magento\Framework\Event\ObserverInterface
         
         $bsecureOrderCheckoutUrl = !empty($additionalData['_bsecure_order_checkout_url']) ?
                                     $additionalData['_bsecure_order_checkout_url'] : "";
-
+        
         if (!empty($bsecureOrderCheckoutUrl)
-            && $methodTitle == 'bSecure Payment'
+            && $methodCode == 'bsecurepayment'
             && $bsecureOrderType == 'before_payment_gateway'
         ) {
-
             $additionalData['_bsecure_order_ref'] = '';
             $additionalData['_bsecure_order_type'] = '';
             $additionalData['_bsecure_order_id'] = '';
